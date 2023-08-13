753863da63e1dea3aa9028de86951bb6c94cbdd8
Chakaka <bis19-skalola@poly.ac.mw>
2023-08-13 19:14:37 +0200

    done with home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+<div class="container-fluid">
+    <div class="row bg-primary shadow" style="height: 500px;
+      background-image: url('/images/lap.jpg') 
+       background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            background-attachment:fixed">
+            <div class="d-flex justify-content-center align-items-center " >
+              <div class="text-white text-center">
+                <h1 class="mb-3">Welcome to Blantyre Secondary School</h1>
+                <h5 class="mb-4">ONLINE PAST PAPERS REPOSITORY</h5>
+                <a class="btn btn-outline-light btn-lg m-2" href="#contact"
+                  role="button" rel="nofollow" >Contact Us</a>
+                <!-- <a class="btn btn-outline-light btn-lg m-2" href="https://mdbootstrap.com/docs/standard/"
+                  target="_blank" role="button">Download MDB UI KIT</a> -->
+              </div>
             </div>
-        </div>
+           
+
     </div>
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="row bg-white" >
+
+        <div class="section-title d-flex justify-content-center text-primary py-2 ">
+          <h2>About Us</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+            </ul>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <a href="#" class="btn-learn-more">Learn More</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+     
+
+   
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,15 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+      <script src="{{ asset('aos-master/dist/aos.js') }}" ></script>
+       <link href="{{ asset('aos-master/dist/aos.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    
+    
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -31,10 +36,10 @@
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body >
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="overflow: hidden;
-        position:fixed;top:0; width:100%;">
-            <div class="container">
+   
+    <div id="app" >
+        <nav class="navbar navbar-expand-md navbar-light bg-body-secondary white shadow-sm" >
+            <div class="container-fluid " >
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     <img src="/images/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
                     BSS-E-Papers
@@ -46,6 +51,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                         <li class="nav-item">
+                                    <a class="nav-link active" href="/">Home</a>
+                         </li>
+                         <li class="nav-item">
+                                    <a class="nav-link" href="/">Disscussion</a>
+                         </li>
+                       
 
                     </ul>
 
@@ -87,10 +99,134 @@
                 </div>
             </div>
         </nav>
+        
+        @guest
+        @else
+         <nav class="navbar navbar-expand-md navbar-light bg-white  shadow-sm"  style="height: 45px">
+            <div class="container-fluid">
+               
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse bg-white   justify-content-center" id="navbarSupportedContent2"
+                style="font-family:Verdana, Geneva, Tahoma, sans-serif;font-size:13px;"
+                >
+                    
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav  ">
+                        <li class="nav-item">
+                                    <a class="nav-link" href="/">Biology</a>
+                         </li>
+                         <li class="nav-item">
+                                    <a class="nav-link" href="/">English</a>
+                         </li>
+                         <li class="nav-item">
+                                    <a class="nav-link" href="/">Biology</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Mathmatics</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Life-skills</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Geography</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Social-Studies</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Mathmaticks</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Physics</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Chemestry</a>
+                         </li>
+                         <li class="nav-item">
+                                <a class="nav-link" href="/">Home-economics</a>
+                         </li>
+                        
+                         
+                     
+                      
+                     
+                                
+                           
+
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            
+        @endguest
 
         <main class="">
             @yield('content')
         </main>
+        @guest
+            @else
+            <footer class="container-fluid bg-dark mt-5 text-white">
+                 
+                 <div class="footer-row row  g-4 py-1" style="color:#abafb1;">
+                    <div class="col-md align-items-center">
+                      <div>
+                        <div class=" h4 m-0 text-blue fw-bold">BSS PAST PAPERS REPSITORY</div>
+
+                        <p>Want to know more about us? Contact us on the following</p>
+                      </div>
+                      <div class="small">
+                             <div class="d-flex align-items-center ">
+                                <button class="btn btn-floating bg-primary btn-sm  me-3 text-white" style="font-size:12px"><i class="fa fa-map-marker"></i></button>
+                                <div style="color:#abafb1;" class="p-0">Our offices are located in Chitawila, Blantyre.</div>
+                             </div>
+                             <div class="d-flex mt-2 align-items-center " id="contact">
+                                <button class="btn btn-floating bg-primary btn-sm me-3 text-white" style="font-size:12px"><i class="fa fa-phone"></i></button>
+                                <div class="div">
+                                <div class="m-0">+265(0) 991 394 987</div>
+                                <div class="m-0">+265(0) 882 2751360</div>
+                                </div>
+                             </div>
+                             <div class="d-flex mt-2 align-items-center ">
+                                <button class="btn btn-floating bg-primary btn-sm me-3 text-white" style="font-size:12px"><i class="fa fa-envelope"></i></button>
+                                <div class="m-0">info@bss.com</div>
+                             </div>
+                               
+                      </div>
+                      
+                    </div>
+                   
+                     
+                   <div class="col-md small ">
+                       <div class="h4 text-white border-1 border-primary border-2 pb-2">Request Past Paper</div>
+                        
+                          
+                               
+                                <div class="form-outline text-white">
+                                    <textarea name="" class="form-control mt-2 text-white border-white" id="descriptionMessage" cols="30" rows="4" style="background:transparent;"></textarea>
+                                      <label for="" class="form-label text-white">Description</label>
+                                </div>
+                                <button class="mt-2 btn btn-primary text-white" style="font-size:12px" id="sendMessage">Send Request </button>
+                          
+                   </div>
+                 </div>
+                 
+                 <div class="mt-4 small border-2 border-top text-center border-white ">
+                    &copy;2023 Copyright Blantyre Secondary School
+                 </div>
+
+                 
+</footer>
+
+            
+            
+        @endguest
+
+        
         
     </div>
 </body>
